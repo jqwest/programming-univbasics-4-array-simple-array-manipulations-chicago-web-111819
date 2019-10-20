5987a274d8832c491038afa3400386f45af4995d
john L <johnleavell@gmail.com>
2019-10-20 22:21:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,9 +43,9 @@ def using_flatten(array)
 
 end
 
-#def using_delete(array,"word")
-#  array.delete()
-#end
+def using_delete(array,"word")
+  array.delete(2)
+end
 
 #def using_delete_at(array, integer)
 
